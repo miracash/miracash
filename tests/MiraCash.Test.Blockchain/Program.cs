4f1c1578a4ae3c527b0442ef54c59cfaaa305315
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-07-25 14:44:15 +0200

    feat(test): blockchain logics test.

---
+++ tests/MiraCash.Test.Blockchain/Program.cs
@@ -0,0 +1,32 @@
+﻿using MiraCash.Blockchain;
+
+public class Program
+{
+    public static void Main()
+    {
+        Blockchain blockchain = new Blockchain();
+        
+        blockchain.AddTransaction(new Transaction("lead", "Bob", 50));
+        blockchain.AddTransaction(new Transaction("Bob", "Charlie", 30));
+        
+        blockchain.MineBlock();
+
+
+        Console.WriteLine("Balance of lead: " + blockchain.GetBalance("lead"));
+        Console.WriteLine("Balance of Bob: " + blockchain.GetBalance("Bob"));
+        Console.WriteLine("Balance of Charlie: " + blockchain.GetBalance("Charlie"));
+
+        var newTransaction = new Transaction("lead", "Charlie", 20);
+
+        if (blockchain.AddTransaction(newTransaction))
+        {
+            blockchain.MineBlock();
+        }
+        else
+        {
+            Console.WriteLine("Transaction is invalid due to insufficient funds.");
+        }
+
+        blockchain.DisplayBlockchain();
+    }
+}
