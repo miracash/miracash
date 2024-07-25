61f24d2c59d484d913fcbc4b9a009a8be5ea4c43
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-07-25 14:08:36 +0200

    feat(blockchain): Transaction.cs, Block.cs and BlockHeader.cs added.

--- src/MiraCash.Blockchain/Block.cs
+++ src/MiraCash.Blockchain/Block.cs
@@ -1,6 +1,39 @@
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace MiraCash.Blockchain;
 
 public class Block
 {
+    public BlockHeader Header { get; set; }
+    public List<Transaction> Transactions { get; set; }
+
+    public string CalculateHash()
+    {
+        string raw = $"{Header.TimeStamp}{Header.MerkleRoot}{Header.PreviousHash}";
+        foreach (var transaction in Transactions)
+        {
+            string transactionDataRaw = $"{transaction.From}{transaction.To}{transaction.Value}";
+            raw += transactionDataRaw;
+        }
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] inputHash = Encoding.UTF8.GetBytes(raw);
+            byte[] hash = sha256.ComputeHash(inputHash);
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+    }
     
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Block(PreviousHash: '{Header.PreviousHash}', Timestamp: {Header.TimeStamp}, MerkleRoot: '{Header.MerkleRoot}', Nonce: {Header.Nonce}, Hash: '{Header.Hash}')");
+        sb.AppendLine("Transactions:");
+        foreach (var transaction in Transactions)
+        {
+            sb.AppendLine(transaction.ToString());
+        }
+        return sb.ToString();
+    }
 }
--- src/MiraCash.Blockchain/Transaction.cs
+++ src/MiraCash.Blockchain/Transaction.cs
@@ -2,5 +2,14 @@ namespace MiraCash.Blockchain;
 
 public class Transaction
 {
-    
+    public string From { get; set; }
+    public string To { get; set; }
+    public decimal Value { get; set; }
+
+    public Transaction(string from, string to, int value)
+    {
+        From = from;
+        To = to;
+        Value = value;
+    }
 }
