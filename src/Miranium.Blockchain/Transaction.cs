954c2747d867925ca5296c0c7c905d354e2940be
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-07-25 18:31:19 +0200

    feat(blockchain): Wallet.cs and Transaction.cs stable implementation.

--- src/Miranium.Blockchain/Transaction.cs
+++ src/Miranium.Blockchain/Transaction.cs
@@ -1,26 +1,26 @@
 using System.Security.Cryptography;
 using System.Text;
 
-namespace MiraCash.Blockchain;
+namespace Miranium.Blockchain;
 
 public class Transaction
 {
-    public string From { get; set; }
-    public string To { get; set; }
+    public string FromAdress { get; set; }
+    public string ToAdress { get; set; }
     public decimal Value { get; set; }
     public string Hash { get; set; }
 
     public Transaction(string from, string to, int value)
     {
-        From = from;
-        To = to;
+        FromAdress = from;
+        ToAdress = to;
         Value = value;
         Hash = CalculateHash();
     }
     
     private string CalculateHash()
     {
-        string raw = $"{From}{To}{Value}";
+        string raw = $"{FromAdress}{ToAdress}{Value}";
 
         using (SHA256 sha256 = SHA256.Create())
         {
@@ -28,5 +28,10 @@ public class Transaction
             byte[] hashBytes = sha256.ComputeHash(inputBytes);
             return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
         }
+    }
+
+    public override string ToString()
+    {
+        return $"Transaction : sender : {FromAdress} -> {Value} to : {ToAdress}";
     }
 }
